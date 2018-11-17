0b3466072312a4383abd9e9780e5a65d22adf0e0
StefCon23 <webman@conorstefanini.local>
2018-11-17 15:50:18 +0000

    working good with rest-like functionality, moving project over to newsr_node, last update before project turns in to the view component, focusing on react

--- cst_node.js
+++ cst_node.js
@@ -27,6 +27,8 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 //	jsdom ...
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+//	saving files
+const fs = require('fs');
 
 
 
@@ -36,87 +38,57 @@ const { JSDOM } = jsdom;
 
 //	getURL, to get a url, using xmlhttprequest
 function getURL(url, callback) {
+	console.log("getting url; " + url + "; now");
 	var request = new XMLHttpRequest();
 	request.open("GET", url, false);
+	//	some issues with character sets, think it's good w/ iso
 	request.setRequestHeader ("Accept", "text/xml; charset=iso-8859-1");
 	request.onreadystatechange = function() {
 		if (request.readyState == 4) {
 			console.log("status; " + request.status);
-			switch (request.status) {
-				case "200":
-					console.log("noice; " + request.responseText);
-					break;
-				case 301: 
-					console.log("status 301 from; " + url);
-					//console.log("char at 4 is; " + url.charAt(4));
-					if (url.charAt(4) == ':') {
-						console.log("try a https");
-						getHTTP("https://" + url.substr(7), callback);
-					}
-					break;
-				default:
-					console.log("oop, non accounted for status; " 
-						+ request.status);
-					break;
+			if (request.status == "200") {
+				//console.log("successful 200 response; " + request.responseText);
+				console.log("successful 200 response, got html");
+			} else {
+				console.log("non accounted for response status; " 
+					+ request.status);
+				console.log("requested url (" + url + ") failed");
+				return "fail";
 			}
-			if ((request.status == "301") && (url.charAt(5) == ':')) {
-				//	this is just stupid
-				url = "https://" + url.substr(7);
-console.log('btw, you might get in an infinite (recursive) loop');
-				//	another ...
-				getHTTP(url, callback);
-			}
-			//	deal with otehr headers, 404, 302, etc.
 		}
 	}
 	request.send();
-
-	console.log(request.responseText);
+	//console.log(request.responseText);
 	callback(request.responseText);
+	console.log("finished getting url; " + url + "\n");
+	return "success";
 }
 
-function htmolestATweet(rawTwatShite) {
-	//	this is your actual disgusting regex html madness
-	//return probably some html
-	//	actually maybe make an object / array of objects
-	//	var tweets = [{user:"", tweet:"", rtsetc:""}];
+var getKeys = function(obj){
+   var keys = [];
+   for(var key in obj){
+      keys.push(key);
+   }
+   return keys;
+}
 
-	//	structure of a user's twitter page, and tweet 'stream' is;
-	//	body ... > div id=doc > div id=page-outer > div id=page container
-	//		> ....... > ol id="stream-items-id"
+//	get tweet's link
+//		people seem to tweet links as a tweet's centre piece (or photos,
+//		but can treat the same way) but the tweet format has very long
+//		html tag attributes etc., need to strip to basic <a> tag
+function getTweetsLink(tweet) {
+	//	check for html tags, get attributes
+}
 
-	//	jquery stuff
-//	var data = $($.parseXML(rawTwatShite)).children('html');
-//	return ($(data).find("stream-items-id").text());
-
-	//	need to check out cheerio
-	//const $ = cheerio.load(rawTwatShite);
-//	var message = "getting through taht twwet stream; \n" + $('stream-items-id').text() + "\n; sipped";
-	//var mess = $('fontcol.message').text();
-	//console.log("getting through taht twwet stream; \n" + mess + "\n; sipped");
-
-
-
+//	htmolest a tweet, handes twitter html pages to get tweets out
+function htmolestATweet(rawTwatShite, format = "json") {
+console.log("starting to parse tweet html now")
 	//	return message
-	var mess = "empty";
+	var mess;
+	var hot = "empty";
 
 	//	works on cst.com to get main banner
 	const dom = new JSDOM(rawTwatShite);
-//	mess = dom.window.document.getElementById("fontcol").textContent;
-
-	//	check if request is a tweet, a user, or a search or whatever
-	//	currently assuming it's a user's page and a request for the 
-	//		n most recent tweets of that user
-	//	twitter user's page lists its tweets in a <div id="stream"
-/*
-	mess = dom.window.document.querySelector("js-tweet-text-container");
-	if (mess != null) {
-		return mess.textContent;
-	} else {
-		return null;
-	}
-*/
-
 
 	var classes = dom.window.document.getElementsByClassName("js-tweet-text-container");
 	/*
@@ -167,58 +139,108 @@ function htmolestATweet(rawTwatShite) {
 	var username;
 	var fullname;
 	var tweet;
+	var time;
 	var replies;
 	var retweets;
-	var favourites;
+	var likes;
 
-	mess = "";
+	hot = "";
+	mess = {
+		time : Date.now(),
+		twts : []
+	}
 
 	for (var i = 0; i < classes.length; ++i) {
+
+console.log("--tweet " + i + " of " + classes.length);
+for (var j = 0; j < classes[i].parentElement.children.length; ++j) {
+	/*	there's how to do links / quotes / pictures or whatever in 
+	**		tweets, check length of classes[i].parentElement.children, 
+	**		if greater than 3 (header, tweet-text, xxx, n xxx, footer)
+	*/
+	console.log("clss " + j + "; " + classes[i].parentElement.children[j].className);
+}
+console.log("tweel elms; " + classes[i].parentElement.children.length);
 		header = classes[i].parentElement.getElementsByClassName("stream-item-header")[0].getElementsByClassName("account-group js-account-group js-action-profile js-user-profile-link js-nav")[0];
 
 		username = header.getElementsByClassName("username u-dir u-textTruncate")[0].innerHTML;
 		//	they put <b> tags on the handle on the twitter site
-		//username = username.replace("<b>\|<//b>","");
 		username = username.replace("<b>","");
 		username = username.replace("</b>","");
+console.log("got username; " + username);
 
 		fullname = header.getElementsByClassName("FullNameGroup")[0].getElementsByClassName("fullname show-popup-with-id u-textTruncate ")[0].innerHTML;
+console.log("got fullname; " + fullname);
 
 		tweet = classes[i].firstElementChild.innerHTML;
-
+console.log("got tweet; " + tweet);
 		//	todo; get any links in the tweet and reformat to be nice
 
-		//	todo; get time; "stream-item-header" > "time"
+		//	time
+		header = classes[i].parentElement.getElementsByClassName("stream-item-header")[0].getElementsByClassName("time")[0].getElementsByClassName("tweet-timestamp js-permalink js-nav js-tooltip")[0];
+		time = header.getAttribute("title");
+		epoch = header.getElementsByClassName("_timestamp js-short-timestamp ")[0].getAttribute("data-time");	
+console.log("got time; " + time + "; epoch; " + epoch);
 
 		//	accessing the footer to get retweets etc.
 		header = classes[i].parentElement.getElementsByClassName("stream-item-footer")[0].getElementsByClassName("ProfileTweet-actionCountList u-hiddenVisually")[0];
 
 		replies = header.getElementsByClassName("ProfileTweet-action--reply u-hiddenVisually")[0].getElementsByClassName("ProfileTweet-actionCount")[0].getElementsByClassName("ProfileTweet-actionCountForAria")[0].innerHTML;
+console.log("got replies; " + replies);
+
 		retweets = header.getElementsByClassName("ProfileTweet-action--retweet u-hiddenVisually")[0].getElementsByClassName("ProfileTweet-actionCount")[0].getElementsByClassName("ProfileTweet-actionCountForAria")[0].innerHTML;
-		favourites = header.getElementsByClassName("ProfileTweet-action--favorite u-hiddenVisually")[0].getElementsByClassName("ProfileTweet-actionCount")[0].getElementsByClassName("ProfileTweet-actionCountForAria")[0].innerHTML;
+console.log("got retweets; " + retweets);
+
+		likes = header.getElementsByClassName("ProfileTweet-action--favorite u-hiddenVisually")[0].getElementsByClassName("ProfileTweet-actionCount")[0].getElementsByClassName("ProfileTweet-actionCountForAria")[0].innerHTML;
+console.log("got likes; " + likes);
+
+
+
+
+
 		//	replies, rt's etc. are currently stored in text, i.e.;
 		//		replies = "20,000 replies"
 		//	leaving it for the moment, but that string.replace() funct
 		//		apparently takes regex, soooo replies.replace("/[^0-9]/g, '')
+//console.log( replies.replace(/[^0-9]/g, '') + " is just a number, I checked" );
 
-		mess += "\n---\n" + fullname + " (" + username + "); " + tweet + "; " + replies + ", " + retweets + ", " + favourites + "\n";
+		hot += "\n---\n" + fullname + " (" + username + "); " + tweet + "; " + replies + ", " + retweets + ", " + likes + "\n";
+		mess.twts.push({
+			username : username,
+			fullname : fullname,
+			tweet : tweet,
+			time : time,
+			epoch : epoch, 
+			replies : replies,
+			retweets : retweets,
+			likes : likes
+		});
+		//console.log(JSON.stringify(mess.twts[i]));
 	}
+	console.log("finishing html tweet parse now")
 
-	for (var i = 0; i < classes.length; ++i) {
-		//mess += "\n---\n";
-		//mess += classes[i].firstElementChild.innerHTML;
+	if (format == "text") {
+		return hot;
+	} else {
+		return mess;
 	}
-
-
-	return mess;
 }
-
+/*
+function htmlFormatTweets(tweets) {
+	var message = "";
+	var delim = "\n---\n";
+	for (var i = 0; i < tweets.length; ++i) {
+		message += tweets[i].fullname + " (" + tweets[i].username + ") " + tweets[i].
+	}
+	return message;
+}
+*/
 
 /*	actual server,      ENTRY POINT ------------------------------------
 */
 const server = http.createServer((req, res) => {
 	//	print request url
-	console.log(Date.now().toString());
+	console.log("starting up at; " + Date.now().toString());
 	console.log("request is; " + req.url);
 	//	going to be dicking with url's soon
 
@@ -228,35 +250,63 @@ const server = http.createServer((req, res) => {
 
 	//	main cst.node page, just return name
 	if (req.url == '/') {
-		res.end('CONOR STEFANINI .NODE\n');
+		var message = "CONOR STEFANINI .NODE\nnothing provided, use "
+		message += "the format /twt?username"
+		console.log("serving the message; " + message);
+		res.end(message);
 	} else {
-		//	a different request supplied via url
-
-		//	bit of a durty way to strip strings
-		//		-5 would give last 5 chars, -1 gives last 1 char
-		//	taking the first char slash of the request name anyway
+		//	a request supplied via url
+		//	taking the first char (slash) off the request
 		var url = req.url.substr(1);
-console.log(url);
-		if (url.charAt(0) != 'h') {	//	genius url error checking
-			url = "https://" + url;	//	probably need https
+
+		//	request checking, implementing protocol
+		//		twt		twitter
+		//			usr	user
+		//		rss		rss
+		//			url	url
+		//	in the format;
+		//		cst.node/twt?usr=everysnake
+		//		cst.node/rss?url=realdonaldtrump
+
+		//	everything before ?
+		var proto = url.slice(0, url.indexOf("\?"));
+		//	everything after ?
+		var u = url.substr(url.indexOf("\?") + 1);
+		//	todo; test with multiple ?'s (???)
+		console.log("got a request for; " + proto + ", and; " + u);
+
+		if (proto == "twt") {
+			url = "https://twitter.com/" + u;
+			console.log("getting some tweets from; " + url);
+			getURL(url, function(out){
+				console.log("running callback on html request");
+				//	todo; find out how res end does it's do, maybe send 
+				//		a full packaged html
+				//res.end("finishing that callback; \n" + out + "\n; (that was it)\n");
+
+/*
+				//	saving response
+				var file = "/temp";
+				fs.writeFile(file, out, function(err) {
+					if (err) {
+						console.log("error saving file; " + file);
+						console.log("error message; " + err);
+						return console.log(err);
+					}
+					console.log("the file; " + file + " was saved");
+				});
+*/
+
+				var message = htmolestATweet(out, "text");
+				//console.log(JSON.stringify(message));
+				//res.end(JSON.stringify(message));
+				res.end(message);
+			});
+		} else if (proto =="rss") {
+			res.end("well then I'd do something rss related");
+		} else {
+			res.end("can't go wrong with a CONOR STEFANINI .NODE (not going to do anything with " + req.url + ")");
 		}
-
-		//	works beautiful, commented out for testing of string munge
-		getURL(url, function(out){
-			console.log("doing the get url callback now");
-			//	todo; find out how res end does it's do, maybe send a 
-			//		full packaged html
-			//res.end("finishing that callback; \n" + out + "\n; (that was it)\n");
-			var final = "finishing that callback; \n" + out + "\n; (that was it)\n";
-
-			//	new tweet handler
-			var donald = htmolestATweet(out);
-			final += "after a munge; \n" + donald + "\n and that's the end of that";
-
-			//	send output to the screen
-			res.end(final);
-			console.log("donezo");
-		});
 
 	}
 });
